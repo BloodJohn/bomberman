e55c6fc929a2b614ac74d35e78bf49fc7f053fe5
Eugene Pavlov <bjohn.ext@gmail.com>
2019-01-31 09:31:41 +0300

    add bomb

---
+++ Assets/Script/BombController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"OnCollision {collision.collider.name}");
+
+        Destroy(gameObject, 1f);
+    }
+}
---
+++ Assets/Script/LevelController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject BombPrefab;
+    private const float heightDropBomb = 5f;
+    private readonly Vector3 maxPos = new Vector3(5f, 0f, 5f);
+
+    private const float delayDropBomb = 2f;
+    private float timer;
+
+    void Start()
+    {
+        Random.InitState(DateTime.Now.Second);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > delayDropBomb)
+        {
+            timer = 0f;
+            CreateBomb();
+        }
+    }
+
+
+    private BombController CreateBomb()
+    {
+        var pos = new Vector3(
+            Random.Range(-maxPos.x, maxPos.x),
+            heightDropBomb,
+            Random.Range(-maxPos.y, maxPos.y));
+
+        var newItem = Instantiate(BombPrefab, transform);
+
+        newItem.transform.position = pos;
+
+        return newItem.GetComponent<BombController>();
+    }
+}
